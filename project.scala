12903fe03311d55ac1864aaf7999fc44922e9ba5
Yaroslav Sydorenko <ysydorenko@evolution.com>
2025-05-19 20:34:07 +0300

    Update CE testkit version.

--- project.scala
+++ project.scala
@@ -4,5 +4,5 @@
 //> using dep co.fs2::fs2-core::3.12.0
 //> using dep co.fs2::fs2-io::3.12.0
 //> using test.dep org.typelevel::munit-cats-effect::2.1.0
-//> using test.dep org.typelevel::cats-effect-testkit::3.6.0
+//> using test.dep org.typelevel::cats-effect-testkit::3.6.1
 //> using dep com.lihaoyi::scalatags::0.13.1
